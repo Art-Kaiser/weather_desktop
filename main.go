8b7b0096ce9c388fb25136d567d850ff4698fc6e
Art-Kaiser <artyon.95@yandex.ru>
2022-04-03 11:28:51 +0300

    add method api/add struct for method

--- main.go
+++ main.go
@@ -11,7 +11,12 @@ func main() {
 	configs.InitConfig()
 
 	if apiKey, exists := os.LookupEnv("API_WEATHER_KEY"); exists {
+		forecast := new(api.WeatherForecast)
+		sity := new(api.CoordinatesCity)
+		//test api
 		res := api.GetWeatherResult(apiKey)
+		api.GetWeathersResult(apiKey, forecast)
+		api.GetCoordinatesCity(apiKey, "Moskow", sity)
 		scripts.InitUI(res)
 	}
 }
