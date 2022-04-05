3e4df16d8dbde3c8636d4c7ec134f1c4fdbbc91a
Art-Kaiser <artyon.95@yandex.ru>
2022-04-05 21:44:06 +0300

    test render/func

--- scripts/buildUI.go
+++ scripts/buildUI.go
@@ -14,15 +14,7 @@ import (
 	"weatherDesktop/api"
 )
 
-func InitUI(res *owm.CurrentWeatherData, forecast *api.WeatherForecast, input *widget.Entry) {
-	app := app.New()
-	window := app.NewWindow("Watch the weather")
-	window.Resize(fyne.NewSize(550, 450))
-	window.SetFixedSize(true)
-	window.SetMaster()
-
-	iconApp, _ := fyne.LoadResourceFromPath("./assets/icon.png")
-
+func renderIconWeather(res *owm.CurrentWeatherData) *fyne.Container {
 	weatherIcon := container.NewGridWrap(
 		fyne.NewSize(160, 160),
 		canvas.NewImageFromFile(
@@ -37,10 +29,81 @@ func InitUI(res *owm.CurrentWeatherData, forecast *api.WeatherForecast, input *w
 		contentCenter,
 	)
 
-	window.SetIcon(iconApp)
+	return weatherIconWrapper
+}
+
+func renderPanelTop(res *owm.CurrentWeatherData, app fyne.App, input *widget.Entry) *widget.Card {
+	leftBox := container.NewVBox(
+		container.NewHBox(
+			widget.NewLabel(fmt.Sprintf("Погодные условия: %s", res.Weather[0].Description)),
+		),
+		container.NewHBox(
+			widget.NewLabel(fmt.Sprintf("Температура: %v C°", math.Round(res.Main.Temp))),
+		),
+		container.NewHBox(
+			widget.NewLabel(fmt.Sprintf("Ветер: %v м/сек", math.Round(res.Wind.Speed))),
+		),
+		container.NewHBox(
+			widget.NewLabel(fmt.Sprintf("Влажность: %d%%", res.Main.Humidity)),
+		),
+		widget.NewButton("Поменять город", func() {
+			window2 := app.NewWindow("Выбор города")
+			window2.Resize(fyne.NewSize(265, 115))
+			window2.SetFixedSize(true)
+
+			window2.SetContent(container.NewVBox(
+				widget.NewLabel("Введите Ваш город:"),
+				input,
+				widget.NewButton("Обновить", func() {
+					
+					//Запрос апи
+					window2.Hide()
+				}),
+			))
+			window2.Show()
+		}),
+	)
+
+	_, monthRes, dayRes := time.Unix(int64(res.Dt), 0).Date()
+
+	panelTop := widget.NewCard(
+		fmt.Sprintf("Местоположение: %s", res.Name),
+		fmt.Sprintf("Дата: %s %v", monthRes, dayRes),
+		container.NewHBox(
+			leftBox,
+			//weatherIconWrapper,
+			renderIconWeather(res),
+		))
+	return panelTop
+}
+
+func InitUI(res *owm.CurrentWeatherData, forecast *api.WeatherForecast, input *widget.Entry) {
+	app := app.New()
+	window := app.NewWindow("Watch the weather")
+	window.Resize(fyne.NewSize(550, 450))
+	window.SetFixedSize(true)
+	window.SetMaster()
+	iconApp, err := fyne.LoadResourceFromPath("./assets/icon.png")
+	if err != nil {
+		window.SetIcon(iconApp)
+	}
+
+	/*weatherIcon := container.NewGridWrap(
+		fyne.NewSize(160, 160),
+		canvas.NewImageFromFile(
+			fmt.Sprintf("./assets/weather/%s.png", res.Weather[0].Icon),
+		),
+	)
+
+	contentCenter := container.New(layout.NewCenterLayout(), weatherIcon)
+
+	weatherIconWrapper := container.NewGridWrap(
+		fyne.NewSize(375, 150),
+		contentCenter,
+	)*/
 
 	//inputCity := widget.NewEntry()
-	leftBox := container.NewVBox(
+	/*leftBox := container.NewVBox(
 		container.NewHBox(
 			widget.NewLabel(fmt.Sprintf("Погодные условия: %s", res.Weather[0].Description)),
 		),
@@ -78,8 +141,9 @@ func InitUI(res *owm.CurrentWeatherData, forecast *api.WeatherForecast, input *w
 		fmt.Sprintf("Дата: %s %v", monthRes, dayRes),
 		container.NewHBox(
 			leftBox,
-			weatherIconWrapper,
-		))
+			//weatherIconWrapper,
+			renderIconWeather(res),
+		))*/
 
 	bottomBox := container.NewHBox()
 
@@ -114,7 +178,8 @@ func InitUI(res *owm.CurrentWeatherData, forecast *api.WeatherForecast, input *w
 	)
 
 	window.SetContent(container.NewVBox(
-		panelTop,
+		//panelTop,
+		renderPanelTop(res, app, input),
 		panelBottom,
 	))
 
@@ -123,6 +188,13 @@ func InitUI(res *owm.CurrentWeatherData, forecast *api.WeatherForecast, input *w
 			//для запросов по текущему дню
 			//formatted := time.Now().Format("It`s: 3:04:05")
 			fmt.Println("inputCity.Text: ", input.Text)
+			resTest := api.GetWeatherResult("", input)
+
+			window.SetContent(container.NewVBox(
+				//panelTop,
+				renderPanelTop(resTest, app, input),
+				panelBottom,
+			))
 		}
 	}()
 
